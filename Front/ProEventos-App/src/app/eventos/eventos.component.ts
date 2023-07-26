04ba17a750053be7b851d4b1e5ee8c5905d7c275
Rafael Dantas <srdantas.rafael@gmail.com>
2023-07-25 22:41:03 -0300

    Seção 4

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent {
+
+  public eventos : any ;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/evento').subscribe(
+      response =>  this.eventos = response,
+      error => console.log(error)
+   );
+  }
+}
