118a9746fd88b267218b31eecaba84c083b06d28
Rafael Dantas <srdantas.rafael@gmail.com>
2023-08-04 19:30:33 -0300

    Seção 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
-  public eventos : any ;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 100;
+  marginImg: number = 2;
+  exibirImagem: boolean = false;
+  private _filtroLista: string = '';
+
+  public get filtro(): string{
+    return this._filtroLista;
+  }
+
+  public set filtro(value: string){
+    this._filtroLista = value;
+    this.eventos = this.filtro ? this.filtrarEventos(this.filtro) : this.eventosFiltrados;
+  }
+
+  public filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleUpperCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleUpperCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleUpperCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -16,10 +38,14 @@ export class EventosComponent {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response =>  this.eventos = response,
+      response => { this.eventos = response; this.eventosFiltrados = this.eventos;},
       error => console.log(error)
-   );
+  );
   }
 }
